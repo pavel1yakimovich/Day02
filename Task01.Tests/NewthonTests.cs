9bce1adfc753d5e90117d3fa1b1331a77743fd85
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-05 01:28:26 +0300

    refactoring of task01; added MS test for task01

---
+++ Task01.Tests/NewthonTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Task01.Newthon;
+
+namespace Task01.Tests
+{
+    [TestClass]
+    public class NewthonTests
+    {
+        [TestMethod]
+        public void Newthon_n26power4eps0_01_return2_2581()
+        {
+            //Arrange
+            int n = 4;
+            double A = 26, eps = 0.01;
+            double expected = 2.2581;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+
+            //Assert
+            Assert.IsTrue(Math.Abs(expected - result) < eps);
+        }
+
+        [TestMethod]
+        public void Newthon_n26_765power3eps0_01_return2_9625()
+        {
+            //Arrange
+            int n = 3;
+            double A = 26.765, eps = 0.01;
+            double expected = 2.9913;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+
+            //Assert
+            Assert.IsTrue(Math.Abs(expected - result) < eps);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void Newthon_n26_765power4epsMinus0_01_returnArithmeticException()
+        {
+            //Arrange
+            int n = 4;
+            double A = 26.765, eps = -0.01;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void Newthon_n26_765power4eps1_2_returnArithmeticException()
+        {
+            //Arrange
+            int n = 4;
+            double A = 26.765, eps = 1.2;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void Newthon_n26_765powerMinus4eps0_01_returnArithmeticException()
+        {
+            //Arrange
+            int n = -4;
+            double A = 26.765, eps = 0.01;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArithmeticException))]
+        public void Newthon_nMinus26_765power4eps0_01_returnArithmeticException()
+        {
+            //Arrange
+            int n = 4;
+            double A = -26.765, eps = 0.01;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+        }
+
+        [TestMethod]
+        public void Newthon_nMinus26_765power3eps0_01_returnMinus2_9913()
+        {
+            //Arrange
+            int n = 3;
+            double A = -26.765, eps = 0.01, expected = -2.9913;
+
+            //Act
+            double result = NewthonMethod(eps, n, A);
+
+            //Assert
+            Assert.IsTrue(Math.Abs(result - expected) < eps);
+        }
+
+    }
+}
--- Task01/Newthon.cs
+++ Task01/Newthon.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Task02
+namespace Task01
 {
     public static class Newthon
     {
@@ -9,11 +9,11 @@ namespace Task02
             double n0 = 0;
             double nk = 1;
 
-            if (A <= 0 && n % 2 != 0)
+            if (A <= 0 && n % 2 == 0)
             {
                 throw new ArithmeticException();
             }
-            if (n == 0)
+            if (n <= 0 || eps <= 0 || eps >= 1)
             {
                 throw new ArithmeticException();
             }
@@ -21,7 +21,7 @@ namespace Task02
             while (Math.Abs(nk - n0) >= eps)
             {
                 n0 = nk;
-                nk = (1 / n)*((n - 1)*n0 + A/Math.Pow(n0, n - 1));
+                nk = (1.0 / n)*((n - 1)*n0 + A/Math.Pow(n0, n - 1));
             }
             return nk;
         }
