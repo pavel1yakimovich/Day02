0a98907617be981e96cf99582eebac6e7c790455
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-05 20:48:26 +0300

    added ddt for task02

---
+++ Task02.Tests/EuclideanTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Task02.Euclidean;
+
+namespace Task02.Tests
+{
+    [TestClass]
+    public class EuclideanTests
+    {
+        private TestContext testContextInstance;
+
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [DeploymentItem("Task02.Tests\\Cases.xml")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML",
+                      "|DataDirectory|\\Cases.xml",
+                      "Case",
+                       DataAccessMethod.Sequential)]
+
+        [TestMethod]
+        public void SqrtN_PositiveTest()
+        {
+            int arg1 = int.Parse((string)TestContext.DataRow["arg1"]);
+            int arg2 = int.Parse((string)TestContext.DataRow["arg2"]);
+            int result = int.Parse((string)TestContext.DataRow["result"]);
+
+            Assert.AreEqual(EuclideanAlgorithm(arg1, arg2), result);
+        }
+    }
+}
